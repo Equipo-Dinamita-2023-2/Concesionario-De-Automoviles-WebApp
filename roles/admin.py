613ea5a22747f189d28cc17d41256575b0c8685a
Juan David <juan.loaiza.santiago@correounivalle.edu.co>
2023-11-11 20:55:35 -0500

    Eliminar archivos no usados

--- roles/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from core.models import Cliente, RolEmpleado, Empleado
-
-# Register your models here.
-admin.site.register(Cliente)
-admin.site.register(RolEmpleado)
-admin.site.register(Empleado)

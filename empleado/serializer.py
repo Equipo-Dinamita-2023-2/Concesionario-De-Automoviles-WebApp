4e1c3f66e9d32f2cfdeadf5a16f910db27a72b73
Juan David <juan.loaiza.santiago@correounivalle.edu.co>
2023-11-12 12:53:29 -0500

    crud_roles eliminados (No pertenecen a la rama)

--- empleado/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from core.models import Empleado
-
-
-class EmpleadoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Empleado
-        fields = '__all__'
--- rol_empleado/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from core.models import RolEmpleado
-
-
-class RolEmpleadoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = RolEmpleado
-        fields = '__all__'

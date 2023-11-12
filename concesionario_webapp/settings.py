382f08bed0f590de1141bdfb53215c6b91b66c45
Sebastian Munoz <juan.munoz.rojas@correounivalle.edu.co>
2023-11-12 15:35:04 -0500

    se crea rama crud sucursales, config amb virtual

--- concesionario_webapp/settings.py
+++ concesionario_webapp/settings.py
@@ -128,3 +128,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Autorizaciones de cors
+CORS_ALLOWED_ORIGINS = []

dc8d154c91c4b268390e997448a434ef6112cb33
Julian <juldarecar2003@gmail.com>
2023-11-16 10:06:22 -0500

    CDAW-63_CDAW-64_CDAW-65_confirmacion_crud

--- tallerista/serializer.py
+++ tallerista/serializer.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from core.models import Empleado
 
-
+#si
 class TalleristaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Empleado

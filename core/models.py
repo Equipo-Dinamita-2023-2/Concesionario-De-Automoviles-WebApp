c6f579bb48442d91cc09cab148b4b600f264fd21
Juan David <juan.loaiza.santiago@correounivalle.edu.co>
2023-11-29 12:18:15 -0500

    Update models.py

--- core/models.py
+++ core/models.py
@@ -6,14 +6,14 @@ from django.db import models
 class Sucursal(models.Model):
     id_sucursal = models.AutoField(primary_key=True)
     ciudad = models.CharField(max_length=20)
-    direccion = models.CharField(max_length=30)
+    direccion = models.CharField(max_length=50)
     celular = models.CharField(max_length=10)
     fijo = models.CharField(max_length=10)
     correo = models.CharField(max_length=50)
 
     # Mostrar en Django Admin
     def __str__(self):
-        return self.direccion
+        return f"{self.ciudad} - {self.direccion}"
 
 
 class Cliente(models.Model):
@@ -21,10 +21,10 @@ class Cliente(models.Model):
     documento = models.CharField(max_length=10, unique=True)
     nombres = models.CharField(max_length=30)
     apellidos = models.CharField(max_length=30)
-    celular = models.CharField(max_length=10)
-    correo = models.CharField(max_length=50)
-    direccion = models.CharField(max_length=30)
-    ciudad = models.CharField(max_length=15)
+    celular = models.CharField(max_length=10, unique=True)
+    correo = models.CharField(max_length=50, unique=True)
+    direccion = models.CharField(max_length=50)
+    ciudad = models.CharField(max_length=20)
 
     def __str__(self):
         return f"{self.nombres} {self.apellidos}"
@@ -44,8 +44,8 @@ class Empleado(models.Model):
     documento = models.CharField(max_length=10, unique=True)
     nombres = models.CharField(max_length=30)
     apellidos = models.CharField(max_length=30)
-    celular = models.CharField(max_length=10)
-    correo = models.CharField(max_length=50)
+    celular = models.CharField(max_length=10, unique=True)
+    correo = models.CharField(max_length=50, unique=True)
     contrasenha = models.CharField(max_length=20)
     direccion = models.CharField(max_length=30)
     ciudad = models.CharField(max_length=15)
@@ -67,15 +67,14 @@ class TipoVehiculo(models.Model):
 
     # Mostrar en Django Admin
     def __str__(self):
-        return f"{self.marca} {self.modelo}"
+        return f"{self.marca} {self.modelo} "
 
 
 class Vehiculo(models.Model):
     placa = models.CharField(max_length=6, primary_key=True)
     id_tipo_vehiculo = models.ForeignKey(
         TipoVehiculo, on_delete=models.CASCADE)
-    disponibilidad_color = models.BooleanField()
-    disponibilidad_precio = models.BooleanField()
+    tipo_uso = models.CharField(max_length=10)
     url = models.CharField(max_length=500)
 
     # Mostrar en Django Admin
@@ -85,59 +84,75 @@ class Vehiculo(models.Model):
 
 class Repuesto(models.Model):
     id_repuesto = models.AutoField(primary_key=True)
+    nombre_repuesto = models.CharField(max_length=50)
     id_tipo_vehiculo = models.ForeignKey(
         TipoVehiculo, on_delete=models.CASCADE)
     stock = models.IntegerField()
     precio = models.IntegerField()
-    descripcion = models.CharField(max_length=50)
+    url = models.CharField(max_length=500)
+    descripcion = models.CharField(max_length=500)
 
     # Mostrar en Django Admin
     def __str__(self):
-        return f"{self.id_repuesto} {self.id_tipo_vehiculo}"
+        return f"{self.nombre_repuesto} {self.id_tipo_vehiculo}"
 
 
 class Cotizacion(models.Model):
     id_cotizacion = models.AutoField(primary_key=True)
     id_cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
     placa = models.ForeignKey(Vehiculo, on_delete=models.CASCADE)
+    id_empleado = models.ForeignKey(Empleado, on_delete=models.CASCADE)
     fecha_cotizacion = models.DateField()
     dias_vigente = models.IntegerField()
+    precio_cotizado = models.IntegerField()
     id_tipo_vehiculo = models.ForeignKey(
         TipoVehiculo, on_delete=models.CASCADE)
-    descripcion_cotizacion = models.CharField(max_length=50)
 
     # Mostrar en Django Admin
     def __str__(self):
-        return f"{self.id_cotizacion} {self.id_cliente} {self.placa}"
+        return f"{self.id_cotizacion} {self.id_cliente} {self.placa} {self.precio_cotizado}"
 
 
 class Venta(models.Model):
     id_venta = models.AutoField(primary_key=True)
     id_empleado = models.ForeignKey(Empleado, on_delete=models.CASCADE)
+    id_cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
     placa = models.ForeignKey(Vehiculo, on_delete=models.CASCADE)
+    id_cotizacion = models.ForeignKey(Cotizacion, on_delete=models.CASCADE)
     fecha_venta = models.DateField()
+    precio_venta = models.IntegerField()
     id_tipo_vehiculo = models.ForeignKey(
         TipoVehiculo, on_delete=models.CASCADE)
-    descripcion_venta = models.CharField(max_length=50)
 
     # Mostrar en Django Admin
     def __str__(self):
-        return f"{self.id_venta} {self.id_empleado} {self.placa}"
+        return f"{self.id_venta} {self.id_empleado} {self.placa} {self.precio_venta}"
 
 
 class OrdenReparacion(models.Model):
     id_reparacion = models.AutoField(primary_key=True)
     id_cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    cod_cliente = models.CharField(max_length=10)
     placa = models.ForeignKey(Vehiculo, on_delete=models.CASCADE)
     fecha_ingreso = models.DateField()
     id_tipo_vehiculo = models.ForeignKey(
         TipoVehiculo, on_delete=models.CASCADE)
     id_empleado = models.ForeignKey(Empleado, on_delete=models.CASCADE)
-    repuestos = models.CharField(max_length=50)
-    estado_vehiculo = models.CharField(max_length=50)
-    descripcion_trabajo = models.CharField(max_length=100)
-    estado_reparacion = models.BooleanField()
+    lista_repuestos = models.CharField(max_length=500)
+    costo_repuestos = models.IntegerField()
+    costo_trabajo = models.IntegerField()
+    costo_total = models.IntegerField()
+    estado_pago = models.CharField(max_length=10)
+    estado_reparacion = models.CharField(max_length=20)
+    descripcion_trabajo = models.CharField(max_length=500)
 
     # Mostrar en Django Admin
     def __str__(self):
         return f"{self.id_reparacion} {self.id_empleado} {self.id_cliente} {self.placa}"
+
+
+class RepuestosOrdenes(models.Model):
+    id_reparacion = models.ForeignKey(
+        OrdenReparacion, on_delete=models.CASCADE)
+    id_repuesto = models.ForeignKey(
+        Repuesto, on_delete=models.CASCADE)
